8520b70c33ee46610f106935a3ee7709591b34b3
Mohammed Hammoud <hammoud.mohammed@outlook.com>
2026-07-14 15:29:51 +0200

    chore: add ec2 capacity and outputs for ecs ec2 alb lab

--- projects/09-ecs-ec2-alb/terraform/variables.tf
+++ projects/09-ecs-ec2-alb/terraform/variables.tf
@@ -17,3 +17,28 @@ variable "desired_task_count" {
   type    = number
   default = 2
 }
+
+variable "ec2_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ec2_ami" {
+  type    = string
+  default = "ami-amazonlinux2023"
+}
+
+variable "autoscaling_min_size" {
+  type    = number
+  default = 2
+
+}
+variable "autoscaling_desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "autoscaling_max_size" {
+  type    = number
+  default = 4
+}
--- projects/09-ecs-ec2-alb/terraform/vpc.tf
+++ projects/09-ecs-ec2-alb/terraform/vpc.tf
@@ -12,10 +12,11 @@ resource "aws_vpc" "lab" {
 }
 
 resource "aws_subnet" "public" {
-  count             = var.subnet_count
-  vpc_id            = aws_vpc.lab.id
-  cidr_block        = cidrsubnet(aws_vpc.lab.cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count                   = var.subnet_count
+  vpc_id                  = aws_vpc.lab.id
+  cidr_block              = cidrsubnet(aws_vpc.lab.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.project_name}-public-subnet-${count.index}"
