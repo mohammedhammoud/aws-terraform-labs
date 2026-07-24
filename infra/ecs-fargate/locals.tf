3468685cd27a5c215119b41f99dbb26a24f6b1f7
Mohammed Hammoud <hammoud.mohammed@outlook.com>
2026-07-25 00:17:23 +0200

    feat(infra): add ECR repositories for ECS Fargate

---
+++ infra/ecs-fargate/locals.tf
@@ -0,0 +1,16 @@
+locals {
+  ecr_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only the last 10 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = { type = "expire" }
+      }
+    ]
+  })
+}
---
+++ infra/ecs-fargate/variables.tf
@@ -0,0 +1,8 @@
+variable "project_name" {
+  type    = string
+  default = "todo-platform-fargate"
+}
+
+variable "region" {
+  type = string
+}
