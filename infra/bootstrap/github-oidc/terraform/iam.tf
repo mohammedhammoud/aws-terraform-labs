5634a909e5f3284a484bfe575c16e27e16a5f90d
Mohammed Hammoud <hammoud.mohammed@outlook.com>
2026-07-27 22:20:14 +0200

    fix: attach readonly access to github actions plan role

--- infra/bootstrap/github-oidc/terraform/iam.tf
+++ infra/bootstrap/github-oidc/terraform/iam.tf
@@ -28,3 +28,8 @@ resource "aws_iam_role" "github_actions" {
   name               = "${local.name_prefix}-github-actions-${each.key}"
   assume_role_policy = data.aws_iam_policy_document.github[each.key].json
 }
+
+resource "aws_iam_role_policy_attachment" "github_actions_plan_readonly" {
+  role       = aws_iam_role.github_actions["plan"].name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
