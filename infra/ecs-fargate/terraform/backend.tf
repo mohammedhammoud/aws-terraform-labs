3f2d698f5ffbbadce707fc99e34ff5cf04810523
Mohammed Hammoud <hammoud.mohammed@outlook.com>
2026-07-29 11:01:50 +0200

    chore: update terraform backend state bucket account

--- infra/ecs-fargate/terraform/backend.tf
+++ infra/ecs-fargate/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "392938852724-terraform-state-s3"
+    bucket       = "455394301478-terraform-state-s3"
     key          = "aws-terraform-labs/ecs-fargate/dev/terraform.tfstate"
     region       = "eu-north-1"
     encrypt      = true
