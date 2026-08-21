a49ce9933a73f05e76f11b2ba0feb2a78d08ccce
Mohammed Hammoud <hammoud.mohammed@outlook.com>
2026-08-21 22:49:45 +0200

    fix(terraform): add e2e cloudfront and s3 apply permissions

---
+++ infra/bootstrap/terraform/iam-github-actions-ecs-fargate-e2e-cf-s3.tf
@@ -0,0 +1,94 @@
+data "aws_iam_policy_document" "ecs_fargate_e2e_cf_s3" {
+  for_each = local.ecs_fargate_e2e_ci_instances
+
+  statement {
+    sid = "ManageFrontendBucket"
+
+    actions = [
+      "s3:CreateBucket",
+      "s3:DeleteBucket",
+      "s3:DeleteBucketPolicy",
+      "s3:DeleteBucketTagging",
+      "s3:GetBucketAcl",
+      "s3:GetBucketCORS",
+      "s3:GetBucketLocation",
+      "s3:GetBucketLogging",
+      "s3:GetBucketNotification",
+      "s3:GetBucketObjectLockConfiguration",
+      "s3:GetBucketOwnershipControls",
+      "s3:GetBucketPolicy",
+      "s3:GetBucketPublicAccessBlock",
+      "s3:GetBucketRequestPayment",
+      "s3:GetBucketTagging",
+      "s3:GetBucketVersioning",
+      "s3:GetBucketWebsite",
+      "s3:GetEncryptionConfiguration",
+      "s3:GetLifecycleConfiguration",
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket",
+      "s3:ListBucketVersions",
+      "s3:PutBucketPolicy",
+      "s3:PutBucketPublicAccessBlock",
+      "s3:PutBucketTagging",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${data.aws_caller_identity.current.account_id}-${each.value.name_prefix}-frontend",
+    ]
+  }
+
+  statement {
+    sid = "ManageFrontendBucketObjects"
+
+    actions = [
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${data.aws_caller_identity.current.account_id}-${each.value.name_prefix}-frontend/*",
+    ]
+  }
+
+  statement {
+    sid = "ManageCloudFront"
+
+    actions = [
+      "cloudfront:CreateDistribution",
+      "cloudfront:CreateOriginAccessControl",
+      "cloudfront:DeleteDistribution",
+      "cloudfront:DeleteOriginAccessControl",
+      "cloudfront:GetCachePolicy",
+      "cloudfront:GetDistribution",
+      "cloudfront:GetDistributionConfig",
+      "cloudfront:GetOriginAccessControl",
+      "cloudfront:GetOriginAccessControlConfig",
+      "cloudfront:GetOriginRequestPolicy",
+      "cloudfront:ListCachePolicies",
+      "cloudfront:ListOriginAccessControls",
+      "cloudfront:ListOriginRequestPolicies",
+      "cloudfront:ListTagsForResource",
+      "cloudfront:TagResource",
+      "cloudfront:UntagResource",
+      "cloudfront:UpdateDistribution",
+      "cloudfront:UpdateOriginAccessControl",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ecs_fargate_e2e_cf_s3" {
+  for_each = local.ecs_fargate_e2e_ci_instances
+
+  name   = "${each.value.apply_role_name}-cf-s3"
+  policy = data.aws_iam_policy_document.ecs_fargate_e2e_cf_s3[each.key].json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_fargate_e2e_cf_s3" {
+  for_each = local.ecs_fargate_e2e_ci_instances
+
+  role       = aws_iam_role.ecs_fargate_e2e_github_actions_apply[each.key].name
+  policy_arn = aws_iam_policy.ecs_fargate_e2e_cf_s3[each.key].arn
+}
